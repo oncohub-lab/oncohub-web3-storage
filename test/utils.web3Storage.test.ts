a6f86fef2930f0c0284c066d3748219cc2ce7eaa
rskrzydelski <r.skrzydelski@gmail.com>
2023-01-25 13:01:49 +0100

    initialize test with mocha framework and chai assertion lib

---
+++ test/utils.web3Storage.test.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+
+describe('web3Storage', async function () {
+  it('should create client with string as token api', async function () {
+    assert.equal('1', '1');
+  });
+  it('should throw error like: when wrong api token provided or not provided at all', async function () {
+    assert.equal('1', '1');
+  });
+});
