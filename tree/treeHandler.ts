ab312a87bae86085524f3722366cf84f53df7666
rskrzydelski <r.skrzydelski@gmail.com>
2023-02-06 11:54:51 +0100

    add method for add data to tree

--- tree/treeHandler.ts
+++ tree/treeHandler.ts
@@ -27,13 +27,40 @@ class treeHandler {
     const data = await fs.readFile(path.resolve(__dirname, name));
     return JSON.parse(data.toString());
   }
-}
 
-const tree = new treeHandler(schema);
-// tree.create('tree', tree.schema);
-(async function () {
-  const name = await tree.getLast();
-  console.log(name);
-  const file = await tree.load(name);
-  console.log(file);
-})();
+  async add(branch: string, fileName: string, cid: string, tree: Tree) {
+    if (branch.slice(-1) === '/') {
+      branch = branch.slice(0, -1);
+    }
+    const branchLst = branch.split('/');
+
+    if (branchLst[0] == tree.name && branchLst.length === 1) {
+      tree.children.push({
+        name: fileName,
+        CID: cid,
+      });
+      return;
+    }
+
+    if (branchLst[0] === tree.name)
+      throw new Error(`current tree node name and branch node name mismatch`);
+    if (branchLst.length <= 2)
+      throw new Error(
+        `to go deeper into tree, at least 2 items is needed (current and next)`
+      );
+
+    const idx = tree.children.findIndex(
+      (obj) => obj.name === branchLst.slice(1, 2)[0]
+    );
+
+    if (idx === -1)
+      throw new Error(`no such file ${branchLst.slice(1, 2)[0]} inside tree`);
+
+    await this.add(
+      branchLst.slice(1).join('/'),
+      fileName,
+      cid,
+      tree.children[idx]
+    );
+  }
+}
