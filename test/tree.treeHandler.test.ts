f7b5960a7c9583108c6ee61842448f4951a1739d
rskrzydelski <r.skrzydelski@gmail.com>
2023-02-07 09:51:21 +0100

    init tests for treeHandler

---
+++ test/tree.treeHandler.test.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+import treeHandler from '../tree/treeHandler';
+
+describe('tree.TreeHandler', async function () {
+  describe('lets testing...', async function () {
+    it('should be ...', async function () {
+      assert.equal(1, 1);
+    });
+  });
+});
